36483595d6a96b9fb0bd2e1c51b004b5879f0b80
oli-vr-wa <oliver.valido.ramos@gmail.com>
2026-05-08 17:53:56 +0800

    refactor: add a generic statement map for generic bank transactions. Added en-AU culture to fix date parsing issue. Unit tests have been updated

--- LedgerPro.Core/Interfaces/IBankStatementParser.cs
+++ LedgerPro.Core/Interfaces/IBankStatementParser.cs
@@ -1,10 +1,11 @@
 using LedgerPro.Core.Entities;
+using LedgerPro.Core.Enums;
 
 namespace LedgerPro.Core.Interfaces
 {
     public interface IBankStatementParser
     {
         // Pass a Stream to be handled by the API, and the ID of the BankSource to link the transactions to
-        IEnumerable<BankTransaction> Parse(Stream fileStream, Guid bankSourceId);
+        IEnumerable<BankTransaction> Parse(Stream fileStream, Guid bankSourceId, BankType bankType);
     }
 }
---
+++ LedgerPro.Infrastructure/Parsers/GenericStatementMap.cs
@@ -0,0 +1,19 @@
+using CsvHelper.Configuration;
+using LedgerPro.Core.Entities;
+
+namespace LedgerPro.Infrastructure.Parsers
+{
+    public class GenericStatementMap : ClassMap<BankTransaction>
+    {
+        public GenericStatementMap(Guid bankSourceId)
+        {
+            Map(m => m.TransactionDate).Name("Date");
+            Map(m => m.Amount).Name("Amount");
+            Map(m => m.Description).Name("Description");
+            Map(m => m.TransactionType).Name("Transaction Type");
+
+            Map(m => m.BankSourceId).Constant(bankSourceId);
+            Map(m => m.Id).Convert(_ => Guid.NewGuid()); // Generate a new GUID for each transaction
+        }
+    }
+}
