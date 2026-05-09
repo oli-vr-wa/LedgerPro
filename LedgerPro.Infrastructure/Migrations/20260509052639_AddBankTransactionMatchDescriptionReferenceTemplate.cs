ee3406459b31b4d6972130b149bb5e61369bef8d
oli-vr-wa <oliver.valido.ramos@gmail.com>
2026-05-09 13:28:45 +0800

    feat: Add migrations for BankTransactionMapping new properties

---
+++ LedgerPro.Infrastructure/Migrations/20260509052639_AddBankTransactionMatchDescriptionReferenceTemplate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerPro.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBankTransactionMatchDescriptionReferenceTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionTemplate",
+                table: "BankTransactionMappings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceTemplate",
+                table: "BankTransactionMappings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionTemplate",
+                table: "BankTransactionMappings");
+
+            migrationBuilder.DropColumn(
+                name: "ReferenceTemplate",
+                table: "BankTransactionMappings");
+        }
+    }
+}
