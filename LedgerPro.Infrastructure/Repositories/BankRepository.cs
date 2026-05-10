c058d2f078b2d966b28c7880d45fa91509911195
oli-vr-wa <oliver.valido.ramos@gmail.com>
2026-05-10 10:22:17 +0800

    feat: added more access methods to BankRepository for DB access

--- LedgerPro.Infrastructure/Repositories/BankRepository.cs
+++ LedgerPro.Infrastructure/Repositories/BankRepository.cs
@@ -18,13 +18,17 @@ namespace LedgerPro.Infrastructure.Repositories
         /// </summary>
         /// <param name="bankSourceId">The unique identifier of the BankSource to retrieve.</param>
         /// <returns>The BankSource entity if found; otherwise, null.</returns>
-        public async Task<BankSource?> GetBankSourceByIdAsync(Guid bankSourceId)
-        {
-            //var allSources = await _dbContext.BankSources.ToListAsync();
-            ///return allSources.FirstOrDefault(x => x.Id == bankSourceId);
-            return await _dbContext.BankSources.FindAsync(bankSourceId);
-        }
-            
+        public async Task<BankSource?> GetBankSourceByIdAsync(Guid bankSourceId) =>                    
+            await _dbContext.BankSources.FindAsync(bankSourceId);
+        
+
+        /// <summary>
+        /// Adds a new BankSource entity to the database context. This method is used to create a new bank source that can be associated with imported bank statements.
+        /// </summary>
+        /// <param name="bankSource">The BankSource entity to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task AddBankSourceAsync(BankSource bankSource) =>
+            await _dbContext.BankSources.AddAsync(bankSource);    
         
         /// <summary>
         /// Retrieves all BankSource entities from the database. This method is typically used to get a list of all bank sources 
@@ -41,16 +45,33 @@ namespace LedgerPro.Infrastructure.Repositories
         /// <returns>List of BankTransactionMapping entities.</returns>
         public async Task<List<BankTransactionMapping>> GetBankTransactionMappingsAsync() =>        
             await _dbContext.BankTransactionMappings.ToListAsync();
+
+        /// <summary>
+        /// Adds a new BankTransactionMapping entity to the database context. This method is used to create a new mapping rule 
+        /// that defines how certain bank transactions should be categorized and matched to GeneralLedgerItems during the import process.
+        /// </summary>
+        /// <param name="mapping">The BankTransactionMapping entity to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task AddBankTransactionMappingAsync(BankTransactionMapping mapping) =>
+            await _dbContext.BankTransactionMappings.AddAsync(mapping);
         
         /// <summary>
         /// Adds a StatementImport entity to the database context. This method is used during the bank statement import process to create a record of the import operation, 
         /// including details such as the bank source, import date, file hash, and transaction count. 
         /// The StatementImport record can be used for auditing and tracking purposes to keep a history of imported statements.
         /// </summary>
-        /// <param name="statementImport"></param>
-        /// <returns></returns>
+        /// <param name="statementImport">The StatementImport entity to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public async Task AddStatementImportAsync(StatementImport statementImport) =>
             await _dbContext.StatementImports.AddAsync(statementImport);
+
+        /// <summary>
+        /// Retrieves all BankTransaction entities associated with a specific BankSourceId. 
+        /// </summary>
+        /// <param name="bankSourceId">The ID of the bank source for which to retrieve transactions.</param>
+        /// <returns>A list of BankTransaction entities.</returns>
+        public async Task<List<BankTransaction>> GetBankTransactionsAsync(Guid bankSourceId) =>
+            await _dbContext.BankTransactions.Where(t => t.BankSourceId == bankSourceId).ToListAsync();
 
         /// <summary>
         /// Adds a collection of BankTransaction entities to the database context. This method is used during the bank statement import process to add the parsed transactions to the context before saving them to the database.
@@ -61,6 +82,13 @@ namespace LedgerPro.Infrastructure.Repositories
             await _dbContext.BankTransactions.AddRangeAsync(transactions);    
 
         /// <summary>
+        /// Retrieves all GeneralLedgerItem entities from the database. 
+        /// </summary>
+        /// <returns>A list of GeneralLedgerItem entities.</returns>
+        public async Task<List<GeneralLedgerItem>> GetGeneralLedgerItemsAsync() =>
+            await _dbContext.GeneralLedgerItems.ToListAsync();
+
+        /// <summary>
         /// Adds a collection of GeneralLedgerItem entities to the database context. This method is used 
         /// during the bank statement import process to add the generated GeneralLedgerItems (based on matched transactions) 
         /// to the context before saving them to the database.
@@ -69,6 +97,15 @@ namespace LedgerPro.Infrastructure.Repositories
         /// <returns>A task representing the asynchronous operation.</returns>
         public async Task AddGLItemsAsync(IEnumerable<GeneralLedgerItem> ledgerItems) =>
             await _dbContext.GeneralLedgerItems.AddRangeAsync(ledgerItems);
+
+        /// <summary>
+        /// Adds a new GeneralLedgerAccount entity to the database context. 
+        /// This method is used to create a new general ledger account that can be associated with GeneralLedgerItems for categorization and reporting purposes.
+        /// </summary>
+        /// <param name="glAccount">The GeneralLedgerAccount entity to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task AddGLAccountAsync(GeneralLedgerAccount glAccount) =>
+            await _dbContext.GeneralLedgerAccounts.AddAsync(glAccount);
 
         public async Task<int> SaveChangesAsync() =>
             await _dbContext.SaveChangesAsync();
