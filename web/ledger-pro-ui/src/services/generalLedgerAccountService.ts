19364f059446eac2479ec52c41576381ad8c41c2
oli-vr-wa <oliver.valido.ramos@gmail.com>
2026-06-08 08:40:47 +0800

    feat: implement General Ledger Account form with CRUD functionality
    - Updated General Ledger Account form to handle Add/Edit and delete processes

--- web/ledger-pro-ui/src/services/generalLedgerAccountService.ts
+++ web/ledger-pro-ui/src/services/generalLedgerAccountService.ts
@@ -3,5 +3,7 @@ import type { GeneralLedgerAccount } from '../types/general-ledger-account.types
 
 export const generalLedgerAccountService = {
     getAll: () => api.get<GeneralLedgerAccount[]>('/ledger/accounts'),
-    create: (data: Omit<GeneralLedgerAccount, 'id'>) => api.post<GeneralLedgerAccount>('/ledger/account', data)
+    create: (data: Omit<GeneralLedgerAccount, 'id'>) => api.post<GeneralLedgerAccount>('/ledger/account', data),
+    update: (id: number, data: Omit<GeneralLedgerAccount, 'id'>) => api.put<GeneralLedgerAccount>(`/ledger/account/${id}`, data),
+    delete: (id: number) => api.delete(`/ledger/account/${id}`)
 };
